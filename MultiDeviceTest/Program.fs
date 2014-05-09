653331388a22afc8111b3217e226d5ba56d07095
Gabriele Cocco <gabry.cocco@gmail.com>
2014-05-09 20:19:05 +0200

    Support for memcpy on OSX and Linux, Customisable min vector size for on-cpu reduction

--- MultiDeviceTest/Program.fs
+++ MultiDeviceTest/Program.fs
@@ -11,11 +11,17 @@ let main argv =
     // Min and max size
     let minVectorSize = 2L <<< 10
     let maxVectorSize = 8L <<< 20
+    let minMatSize = 64L
+    let maxMatSize = 2048L
     let perTestDuration = 2000.0
 
     // Vector add
     Console.WriteLine(":::::::::::::::: Vector Addition ::::::::::::::::")
     VectorAddTest.DoTest(minVectorSize, maxVectorSize, 100)
+    Console.WriteLine(":::::::::::::::::::::::::::::::::::::::::::::::::")
+
+    Console.WriteLine("::::::::::::: Matrix Multiplication :::::::::::::")
+    MatrixMultTest.DoTest(minMatSize, maxMatSize, 100)
     Console.WriteLine(":::::::::::::::::::::::::::::::::::::::::::::::::")
     Console.Read() |> ignore
     0
--- MultiDeviceTest/VectorAddTest.fs
+++ MultiDeviceTest/VectorAddTest.fs
@@ -16,7 +16,7 @@ let VectorAdd(a: float32[], b: float32[], c: float32[]) =
     c.[gid] <- a.[gid] + b.[gid]
 
 let Verify(r: float32[]) =
-    let v = Array.tryFind(fun (it:float32) -> it <> 50.0f) r
+    let v = Array.tryFind(fun (it:float32) -> it <> (float32)r.Length) r
     v.IsNone
         
 let DoTest(minSize: long, maxSize: long, iters: int) =
@@ -29,18 +29,14 @@ let DoTest(minSize: long, maxSize: long, iters: int) =
                         MemoryFlags.HostReadOnly ||| MemoryFlags.UsePersistentMemAMD ||| MemoryFlags.WriteOnly;
                         MemoryFlags.HostReadOnly ||| MemoryFlags.WriteOnly; ]
 
-    let wr = new StreamWriter("VectorAddition.csv", false)
-    wr.WriteLine("PLATFORM;DEVICE;READ MODE;WRITE MODE;INPUT FLAGS;OUTPUT FLAGS;SIZE;TIME;ITERATIONS")
-    wr.Close()
-    wr.Dispose()
+    let wr = new StreamWriter("VectorAdd.csv", true)
+    //wr.WriteLine("PLATFORM;DEVICE;READ MODE;WRITE MODE;INPUT FLAGS;OUTPUT FLAGS;SIZE;TIME;ITERATIONS")
 
-    for pIndex = 0 to 0 do        
-        Console.WriteLine("Platform: " + pIndex.ToString())
-        for dIndex = 0 to 2 do
-            if dIndex >= 0 then
-                Console.WriteLine(" Device " + ": " + dIndex.ToString() + "(" + dIndex.ToString() + ")")
-                let wr = new StreamWriter("VectorAdd.csv", true)
-
+    for pIndex, pName, pDevs in GetOpenCLPlatforms() do        
+        Console.WriteLine("Platform: " + pName.ToString())
+        for dIndex, dName, dType in pDevs do
+                Console.WriteLine(" Device " + ": " + dName.ToString() + "(" + dType.ToString() + ")")
+                
                 for rm in inputReadModes do
                     Console.WriteLine("  ReadMode: " + rm.ToString())
                     for wm in outputWriteModes do
@@ -52,46 +48,49 @@ let DoTest(minSize: long, maxSize: long, iters: int) =
                                 let size = ref minSize
                                 while !size <= maxSize do
                                     Console.WriteLine("      Size: " + String.Format("{0,10:##########}", !size))
-                                    let a = Array.create (!size |> int) 16.0f
-                                    let b = Array.create (!size |> int) 34.0f
-                                    let c = Array.zeroCreate<float32> (!size |> int)
-
-                                    let comp = <@ DEVICE(pIndex, dIndex,
-                                                    VectorAdd(
-                                                     BUFFER_READ_MODE(rm, 
-                                                        MEMORY_FLAGS(ifl, 
-                                                            a)),
-                                                     BUFFER_READ_MODE(rm, 
-                                                        MEMORY_FLAGS(ifl, 
-                                                            b)),
-                                                     BUFFER_WRITE_MODE(wm, 
-                                                        MEMORY_FLAGS(ofl, 
-                                                            c)))) @>
-                                    // Run once to skip compilation time
-                                    comp.Run(!size, 64L)
-                                    if not (Verify(c)) then
-                                        Console.WriteLine("---------------- COMPUTATION RESULT ERROR")
-                                    
-                                    // Run
-                                    let watch = new Stopwatch()
-                                    watch.Start()
-                                    for i = 0 to iters - 1 do
+                                    if dType <> DeviceType.Cpu || ((ifl &&& MemoryFlags.UsePersistentMemAMD |> int = 0) && (ofl &&& MemoryFlags.UsePersistentMemAMD |> int = 0)) then
+                                        let a = Array.zeroCreate<float32> (!size |> int)
+                                        let b = Array.zeroCreate<float32> (!size |> int)
+                                        let c = Array.zeroCreate<float32> (!size |> int)
+                                        for i = 0 to (!size |> int) - 1 do
+                                            a.[i] <- (float32)i
+                                            b.[i] <- (float32)((!size |> int) - i)
+                                        let comp = <@ DEVICE(pIndex, dIndex,
+                                                        VectorAdd(
+                                                         BUFFER_READ_MODE(rm, 
+                                                            MEMORY_FLAGS(ifl, 
+                                                                a)),
+                                                         BUFFER_READ_MODE(rm, 
+                                                            MEMORY_FLAGS(ifl, 
+                                                                b)),
+                                                         BUFFER_WRITE_MODE(wm, 
+                                                            MEMORY_FLAGS(ofl, 
+                                                                c)))) @>
+                                        // Run once to skip compilation time
                                         comp.Run(!size, 128L)
-                                    watch.Stop()
-                                    let ttime, iters = ((double)watch.ElapsedMilliseconds) /((double)iters), iters
-                                    //let ttime, iters = Utils.ExcuteFor 1000.0 (fun () -> comp.Run(!size, 64L))
+                                        if not (Verify(c)) then
+                                            Console.WriteLine("---------------- COMPUTATION RESULT ERROR")
+                                        else
+                                            // Run
+                                            let watch = new Stopwatch()
+                                            watch.Start()
+                                            for i = 0 to iters - 1 do
+                                                comp.Run(!size, 128L)
+                                            watch.Stop()
+                                            let ttime, iters = ((double)watch.ElapsedMilliseconds) /((double)iters), iters
+                                            //let ttime, iters = Utils.ExcuteFor 1000.0 (fun () -> comp.Run(!size, 64L))
                                 
-                                    Console.WriteLine("---------------- " + String.Format("{0,11:######0.0000}", ttime) + "ms (" + String.Format("{0,10:#########0}", iters) + " iterations)")
-                                    let s = String.Format("{0};{1};{2};{3};{4};{5};{6};{7};{8}", 
-                                                    pIndex, 
-                                                    (dIndex.ToString() +  " " + dIndex.ToString()), 
-                                                    rm, wm, ifl, ofl, 
-                                                    !size,
-                                                    ttime,
-                                                    iters)
-                                    wr.WriteLine(s)
+                                            Console.WriteLine("---------------- " + String.Format("{0,11:######0.0000}", ttime) + "ms (" + String.Format("{0,10:#########0}", iters) + " iterations)")
+                                            let s = String.Format("{0};{1};{2};{3};{4};{5};{6};{7};{8}", 
+                                                            pIndex, 
+                                                            (dIndex.ToString() +  " " + dName.ToString()), 
+                                                            rm, wm, ifl, ofl, 
+                                                            !size,
+                                                            ttime,
+                                                            iters)
+                                            wr.WriteLine(s)
+                                            System.Threading.Thread.Sleep(500)
 
-                                    size := !size * 2L
-                                    
-                wr.Close()
+                                    size := !size * 2L                                    
+    wr.Close()
                     
