c2acd5d341a6d167d6fc627d8a02fef40f566b9a
GabrieleCocco <gabriele@gabrielecocco.it>
2015-06-18 05:11:39 +0200

    Moving sample into source folder

---
+++ src/FeatureExtractionSample/AssemblyInfo.fs
@@ -0,0 +1,12 @@
+﻿namespace System
+open System.Reflection
+
+[<assembly: AssemblyTitleAttribute("FeatureExtractionSample")>]
+[<assembly: AssemblyProductAttribute("FSCL.Runtime")>]
+[<assembly: AssemblyDescriptionAttribute("FSCL kernel scheduling and execution support")>]
+[<assembly: AssemblyVersionAttribute("2.0.1")>]
+[<assembly: AssemblyFileVersionAttribute("2.0.1")>]
+do ()
+
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "2.0.1"
